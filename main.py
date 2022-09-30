d6c773681eb14835f079c1c5126a415dd25b5a54
Pablo <pabarragan94@gmail.com>
2022-09-30 23:38:13 +0200

    copyfile instead of move, cleaning, tested on win

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from xml.etree.ElementInclude import include
-from zipfile import ZipFile
 import os
-from shutil import move
+from zipfile import ZipFile
+from shutil import copyfile
 import configparser
 
-# dont touch values below
 gbc_ext = '.gbc'
 gb_ext = '.gb'
 gb_count = 0
@@ -19,30 +17,31 @@ def get_config(rel_path):
     return config['DEFAULT']
 
 if __name__ == "__main__":
+    # Read config
     CONFIG = get_config('config.ini')
-
-    print(CONFIG)
-
     inputd = CONFIG['input'] # 'input/'
     gb_outputd = CONFIG['gb_output'] # 'GB/'
     gbc_outputd = CONFIG['gbc_output'] # 'GBC/'
-    # iterate over files in
-    # input directory
+    # Iterate over files in input directory
     for filename in os.listdir(inputd):
         f = os.path.join(inputd, filename)
-        # checking if it is a file
+        print(filename)
+        # Checking if it's a zip file
         if os.path.isfile(f) and '.zip' in f:
             # Read zipfile
             with ZipFile(f, 'r') as z:
                 contents = z.namelist()[0]
                 if gbc_ext in contents:
-                    print(gbc_ext)
-                    move(f, gbc_outputd)
+                    # Gameboy color ROM found
+                    copyfile(f, gbc_outputd + filename)
                     gb_count += 1
                 elif gb_ext in contents:
-                    print(gb_ext)
-                    move(f, gb_outputd)
+                    # Gameboy ROM found
+                    copyfile(f, gb_outputd + filename)
                     gbc_count += 1
+    print('========================')
     print('GameBoy: ' + str(gb_count))
     print('GameBoy Color: ' + str(gbc_count))
+    print('========================')
+    print('Script made by pablo. || https://github.com/pbl0')
 
